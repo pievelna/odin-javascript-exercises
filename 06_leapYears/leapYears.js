b2c72bceb178782d5a70ca86c871a90870d3b25c
Roberts Cacus <kas.pie.velna@gmail.com>
2022-08-28 20:03:22 +0300

    leap year conditions with division remainder and else-if

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let result = false;
+  if(year%400 == 0) {
+    result = true;
+  } else if (year%4 == 0 && year%100 > 0) {
+    result = true;
+  }
+  return result;
 };
 
 // Do not edit below this line
