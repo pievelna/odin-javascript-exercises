28b32490df9cd8bd033dc2db25410af3e025563e
Roberts Cacus <kas.pie.velna@gmail.com>
2022-08-28 20:18:06 +0300

    temperature conversion. Hardest part - correct decimal places as a number. JS is weird sometimes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let result = 0;
+  if (typeof(farenheit) == 'number') {
+    result = (farenheit - 32) * (5 / 9);
+  }
+  return +result.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let result = 0;
+  if (typeof(celsius) == 'number') {
+    result = celsius* (9 / 5) + 32;
+  }
+  return +result.toFixed(1);
 };
 
 // Do not edit below this line
