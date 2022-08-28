6a709aa128db49f1e6c0ebef699268dffd0c4107
Roberts Cacus <kas.pie.velna@gmail.com>
2022-08-28 19:23:49 +0300

    added function parameter check to determine the bigger number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
 const sumAll = function(num1,num2) {
   let result = 0;
-  for(i=num1;i<=num2;i++){
+  let rangeStart = num1;
+  let rangeEnd = num2;
+
+  if (num1 > num2) {
+    rangeStart = num2;
+    rangeEnd = num1;
+  }
+
+  for(i = rangeStart;i <= rangeEnd;i++){
     result += i;
   }
   return result;
