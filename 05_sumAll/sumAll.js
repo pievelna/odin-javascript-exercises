4fc0363836e8d9ef92af5265bb70e546fe5b609f
Roberts Cacus <kas.pie.velna@gmail.com>
2022-08-28 19:10:36 +0300

    loop to sum the numbers and numbers inbetween

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+  let result = 0;
+  for(i=num1;i<=num2;i++){
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
